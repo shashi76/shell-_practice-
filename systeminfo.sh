348ab40186cbac89371df3f04f1f66475a77728c
shashi <sonwane.shashi487@gmail.com>
2025-08-04 14:26:47 +0530

    added system info

---
+++ systeminfo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+echo "================System Info ============="
+echo "This is the hostname of system $HOSTNAME"
+
+echo "System info of this machine"
+
+echo "The home directory of system $HOME"
+
+echo "This is the shell of system $SHELL"
+
+echo "It is the present working directory $PWD"
+
+echo "----------------------------------------------"
+
+
